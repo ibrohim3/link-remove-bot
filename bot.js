fc6bd16e873cf5b2e80501d6369a60f56b1f8171
ibrohim3 <ibrohim.2128382@gmail.com>
2026-01-03 23:37:26 +0500

    Initial commit

--- bot.js
+++ bot.js
@@ -1,17 +1,8 @@
-// const TelegramBot = require('node-telegram-bot-api')
-// function createBot(token) {
-//     if (!token) {
-//         throw new Error("BOT_TOKEN topilmadi")
-//     }
-//     return new TelegramBot(token, { polling: true })
-// }
-// module.exports = { createBot }
 import { Bot } from "grammy";
 import { startHandler } from "./src/handlers/start.handler.js";
 import { antiLinkHandler } from "./src/handlers/antiLink.handler.js";
 
 export const bot = new Bot(process.env.BOT_TOKEN);
-
 // Handlerlarni ulash
 startHandler(bot);
 antiLinkHandler(bot);
--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-// require("dotenv").config()
-// const { createBot } = require('./src/bot');
-// const handleMessage = require('./src/handlers/message');
-// const handleStart = require('./src/handlers/start');
-
-// try {
-//     const bot = createBot(process.env.BOT_TOKEN);
-
-//     // /start komandasi
-//     bot.onText(/\/start/, (msg) => {
-//         handleStart(bot, msg);
-//     });
-
-//     // oddiy xabarlar
-//     bot.on('message', (msg) => {
-//         handleMessage(bot, msg);
-//     });
-
-//     console.log('Bot ishga tushdi');
-// } catch (error) {
-//     console.error('Bot start error: ', error.message);
-// }
 
 import "dotenv/config";
-import { bot } from "./bot.js";  // .js ni yozish ESM uchun muhim
+import { bot } from "./bot.js";
 
 bot.start({
     onStart: (info) => {
